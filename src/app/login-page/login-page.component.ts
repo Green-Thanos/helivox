3f3f22b5550d0604baa031d3d6062751bcc4f8b7
Vijay Ravi <vijjr04@gmail.com>
2023-07-15 20:44:33 -0400

    signup logic

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AbstractControl, FormControl, NgForm, ValidationErrors, ValidatorFn, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   signup = false;
   loginForm: FormGroup;
   signupForm: FormGroup;
+
+  unloaded = false;
+
+  authSubscription: Subscription;
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
@@ -29,16 +35,38 @@ export class LoginPageComponent implements OnInit {
     })
   }
 
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
+
   onSubmitLogin(){
+
     console.log(this.loginForm.value);
     this.loginForm.reset();
-    this.router.navigate([""]);
+    // this.router.navigate([""]);
   }
 
   onSubmitSignup(){
-    console.log(this.signupForm.value);
+    if(!this.signupForm.valid){
+      return;
+    }
+    this.unloaded = true;
+    this.authSubscription = this.auth.signup(this.signupForm.value.username, this.signupForm.value.passwordData.password1).subscribe(resData => {
+      console.log(resData);
+      this.unloaded = false;
+      this.signup = false;
+    }, error => {
+      if(error.status === 400){
+        alert("This user already exists!");
+      }
+      else{
+        alert(error.error.error.message)
+        console.log(error);
+      }
+      this.unloaded = false;
+    });
     this.signupForm.reset();
-    this.router.navigate([""]);
+    // this.router.navigate([""]);
   }
 
 
@@ -50,7 +78,7 @@ export class LoginPageComponent implements OnInit {
     return null;
   }
 
-  constructor(private router: Router){
+  constructor(private router: Router, private auth: AuthService){
 
   }
 
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,19 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+
 @Injectable({providedIn: 'root'})
-export class Auth {
+export class AuthService {
+
+    signup(email: string, password: string){
+        return this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDXcpYQ9XHpJSRyhhqJgRx4Mc99eh_MeLE', 
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
+    }
+
+    constructor(private http: HttpClient){}
     
 }
